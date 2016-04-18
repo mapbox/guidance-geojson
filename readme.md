5ab88da008edb9ca59e3610afc172baf5c9ec7c2
Young Hahn <young@mapbox.com>
2016-04-18 15:55:07 -0400

    Sketch of guidance-geojson based route styling

--- readme.md
+++ readme.md
@@ -2,3 +2,52 @@ guidance-geojson
 ----------------
 Project for generating guidance-specific labelling/route styling data for GL from Mapbox Directions responses.
 
+```js
+var guidanceGeoJSON = require('guidance-geojson');
+
+var geojson = guidanceGeoJSON(directionsResponse);
+// console.log(geojson);
+```
+
+### Styling Guidance GeoJSON
+
+Guidance GeoJSON provides a FeatureCollection with the following feature types:
+
+- `segments` are LineString features that together make up the route to be traveled.
+- `labels` are Point features for maneuver points where the user needs to make a turn.
+- `waypoints` are Point features for starting, final, or intermediate destinations.
+
+Each feature has the following properties exposed for styling in Mapbox Studio:
+
+property | segment | label | waypoint
+--- | --- | --- | ---
+`type` | `segment` | `label` | `waypoint`
+`bearing` | `null` | `0-360` | `null`
+`modifier` | `null` | TBD | `null`
+`name` | `null` | Name of next road | Name of waypoint road
+`waypoint` | `null` | `null` | `"first"|"last"|null`
+
+@TODO: provide example Guidance GeoJSON file for styling in studio as a static route.
+
+### Integrating Guidance GeoJSON dynamically with a Mapbox GL JS map
+
+Once you have created your map style you can have it dynamically style a route. To do so, however, you will need to use the `stylePrep()` and `styleRoute()` methods to omit the layers in your style when loading your map and then apply those styles to your dynamic guidance GeoJSON once loaded:
+
+```js
+var guidanceGeoJSON = require('guidance-geojson');
+var mapboxgl = require('mapbox-gl');
+var directionsResponse = require('./stored-directions-response.json');
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    // Defers showing all layers from `style` beginning with `route`
+    style: guidanceGeoJSON.stylePrep(style, 'route')
+});
+
+map.on('style.load', function () {
+    var route = guidanceGeoJSON(directionsResponse);
+    // Restores style layers and adjusts dynamically to style `route` GeoJSON
+    guidanceGeoJSON.styleRoute(mapboxgl, map, route);
+});
+```
+
---
+++ test/style.test.js
@@ -0,0 +1,132 @@
+var fs = require('fs');
+var tape = require('tape');
+var stylePrep = require('../index').stylePrep;
+var styleRoute = require('../index').styleRoute;
+
+tape('stylePrep', function(assert) {
+    var style = JSON.parse(JSON.stringify(require('./fixtures/style-route.json')));
+
+    assert.deepEqual(style.layers.length, 24, 'before: has 24 layers');
+    assert.deepEqual(style.layers.map(function(l) { return l.id; }), [
+        'background',
+        'landuse',
+        'landuse-sand',
+        'water',
+        'building',
+        'building-top',
+        'road-minor-case',
+        'road-extra-case',
+        'road-extra',
+        'road-minor',
+        'tunnel-major-case',
+        'tunnel-major',
+        'road-major-case',
+        'road-major',
+        'route-case',
+        'route',
+        'road-label-extra',
+        'road-label-minor',
+        'road-label-major',
+        'route-spacer',
+        'motorway-shields-other',
+        'motorway-shields-interstate',
+        'route-maneuver-left',
+        'route-maneuver-right'
+    ], 'before: has correct layer IDs');
+    assert.deepEqual(style.metadata.guidanceRoute, undefined, 'before: no guidanceRoute metadata');
+
+    assert.deepEqual(style, stylePrep(style), 'after: returns style');
+    assert.deepEqual(style.layers.length, 19, 'after: has 19 layers');
+    assert.deepEqual(style.layers.map(function(l) { return l.id; }), [
+        'background',
+        'landuse',
+        'landuse-sand',
+        'water',
+        'building',
+        'building-top',
+        'road-minor-case',
+        'road-extra-case',
+        'road-extra',
+        'road-minor',
+        'tunnel-major-case',
+        'tunnel-major',
+        'road-major-case',
+        'road-major',
+        'road-label-extra',
+        'road-label-minor',
+        'road-label-major',
+        'motorway-shields-other',
+        'motorway-shields-interstate',
+    ], 'after: has correct layer IDs');
+    assert.deepEqual(style.metadata.guidanceRoute.map(function(l) { return l.layer.id + '@' + l.before; }), [
+        'route-case@road-major',
+        'route@route-case',
+        'route-spacer@road-label-major',
+        'route-maneuver-left@motorway-shields-interstate',
+        'route-maneuver-right@route-maneuver-left'
+    ], 'after: guidanceRoute has correct layer IDs');
+    assert.end();
+});
+
+tape('styleRoute', function(assert) {
+    assert.throws(function() {
+        styleRoute({}, {getStyle:function() { return {}; }}, {});
+    }, /metadata.guidanceRoute not found/, 'throws without metadata');
+
+    assert.throws(function() {
+        styleRoute({}, {getStyle:function() { return { metadata:{} }; }}, {});
+    }, /metadata.guidanceRoute not found/, 'throws without metadata.guidanceRoute');
+
+    var route = {};
+    var mapboxgl = {};
+    mapboxgl.GeoJSONSource = function(options) {
+        assert.deepEqual(options.data, route, 'route data provided to GeoJSONSource constructor');
+    };
+    var map = {};
+    map.getStyle = function() {
+        return {
+            metadata: {
+                guidanceRoute: [
+                    {
+                        layer: {
+                            id: 'route',
+                            source:'test',
+                            'source-layer':'data'
+                        },
+                        before: 'road'
+                    },
+                    {
+                        layer: {
+                            id: 'route-label',
+                            source:'test',
+                            'source-layer':'data'
+                        },
+                        before: 'road-label'
+                    }
+                ]
+            }
+        };
+    };
+    map.addSource = function(id, source) {
+        assert.equal(id, 'route-guidance', 'addSource: adds source with id=route-guidance');
+        assert.equal(source instanceof mapboxgl.GeoJSONSource, true, 'addSource: adds GeoJSONSource');
+    };
+    map.addLayer = function(layer, before) {
+        if (layer.id === 'route') {
+            assert.equal(layer.source, 'route-guidance', 'layer.source = route-guidance');
+            assert.equal(layer['source-layer'], undefined, 'layer.source-layer is unset');
+            assert.equal(before, 'road', 'layer is added before id:road');
+        } else if (layer.id === 'route-label') {
+            assert.equal(layer.source, 'route-guidance', 'layer.source = route-guidance');
+            assert.equal(layer['source-layer'], undefined, 'layer.source-layer is unset');
+            assert.equal(before, 'road-label', 'layer is added before id:road-label');
+        } else {
+            assert.fail('unknown layer ' + layer.id);
+        }
+    };
+
+    styleRoute(mapboxgl, map, route);
+
+    assert.end();
+});
+
