15028da808ae9505a3c8c7667e23b0f7a8b353a3
Young Hahn <young@mapbox.com>
2016-05-16 13:25:20 -0400

    Update usage in bin test

--- test/bin.test.js
+++ test/bin.test.js
@@ -4,7 +4,7 @@ var exec = require('child_process').exec;
 tape('guidance-geojson: usage', function(assert) {
     exec(__dirname + '/../bin/guidance-geojson.js', function(err, stdout, stderr) {
         assert.equal(err && err.code, 1, 'exits 1 without arg');
-        assert.equal(stdout.trim(), 'Usage: guidance-geojson <file.json>', 'prints usage');
+        assert.equal(stdout.trim(), 'Usage: guidance-geojson <file.json> [--offset=num in km]', 'prints usage');
         assert.end();
     });
 });
