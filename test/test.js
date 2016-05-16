a57d5e1a1f8fa73f903b3bc6eeb8e75280e5cbc1
Young Hahn <young@mapbox.com>
2016-05-16 14:20:20 -0400

    Tests for v4

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var tape = require('tape');
 var guidanceGeojson = require('../index');
+var distance = require('turf-distance');
 
 tape.skip('convert directions v5 response to geojson', function(assert) {
     var input = JSON.parse(fs.readFileSync(__dirname + '/fixtures/v5-sf.json'));
@@ -84,6 +85,27 @@ tape('convert directions v4 response to geojson', function(assert) {
         type: 'waypoint',
         waypoint: 'last'
     }, 'has last waypoint');
+
+    assert.end();
+});
+
+tape('v4 offset', function(assert) {
+    var input = JSON.parse(fs.readFileSync(__dirname + '/fixtures/v4-sf.json'));
+    var offset00 = guidanceGeojson(input, { offset: 0.0 });
+    var offset01 = guidanceGeojson(input, { offset: 0.1 });
+
+    var labelPoints00 = offset00.features
+        .filter(function(f) { return f.properties.type === 'label'; })
+    var labelPoints01 = offset01.features
+        .filter(function(f) { return f.properties.type === 'label'; })
+    var distances = labelPoints01.map(function(a, i) {
+        var b = labelPoints00[i];
+        return distance(a, b, 'kilometers');
+    });
+    assert.ok(Math.abs(0.03-distances[0]) < 0.001, 'label 1 is offset 0.03km');
+    assert.ok(Math.abs(0.10-distances[1]) < 0.001, 'label 2 is offset 0.10km');
+    assert.ok(Math.abs(0.10-distances[2]) < 0.001, 'label 3 is offset 0.10km');
+    assert.ok(Math.abs(0.10-distances[3]) < 0.001, 'label 4 is offset 0.10km');
 
     assert.end();
 });
